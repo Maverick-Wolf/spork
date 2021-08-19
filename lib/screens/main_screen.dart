6fcd71753cbc45de875bad3c14aa4b25da29537a
rachit <rachau76@gmail.com>
2021-08-19 07:46:58 +0530

    got the help and clear commands working

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -7,23 +7,48 @@ class MainScreen extends StatefulWidget {
 
 class _MainScreenState extends State<MainScreen> {
   List<Widget> _list = [];
+  outputText(String value) {
+    Widget text;
+    if (value.toLowerCase() == "help") {
+      text = Padding(
+        padding: const EdgeInsets.fromLTRB(30.0, 0.0, 0.0, 0.0),
+        child: Text(
+          "about\n    - Stop stalking me\n\nskills\n    - Learn to know about all the magical powers i posses\n\nprojects\n    - See all the potions i have made using my magical powers\n\neducation\n    - Learn about the wizidary schools i went to\n\ncontact\n    - Let the spam begin",
+          style: TextStyle(color: Colors.white),
+        ),
+      );
+    } else {
+      text = Padding(
+        padding: const EdgeInsets.fromLTRB(30.0, 0.0, 0.0, 0.0),
+        child: Text(
+          "No a valid input",
+          style: TextStyle(color: Colors.white),
+        ),
+      );
+    }
+    return text;
+  }
 
   @override
   void initState() {
     super.initState();
     addTextField() {
-      bool enabled = true;
       return Padding(
         padding: EdgeInsets.fromLTRB(5.0, 0.0, 0.0, 0.0),
         child: TextField(
-          enabled: enabled,
           style: TextStyle(color: Colors.white, fontSize: 14.0),
           cursorColor: Colors.white,
           autofocus: true,
-          onSubmitted: (value) {
-            print(value);
+          onSubmitted: (value) async {
+            Widget text = await outputText(value);
             setState(() {
-              _list.add(addTextField());
+              if (value.toLowerCase() == "clear") {
+                _list.clear();
+                _list.add(addTextField());
+              } else {
+                _list.add(text);
+                _list.add(addTextField());
+              }
             });
           },
           decoration: InputDecoration(
@@ -66,6 +91,11 @@ class _MainScreenState extends State<MainScreen> {
                 width: MediaQuery.of(context).size.width * 0.8,
                 child: Row(
                   children: [
+                    Image(
+                      image: AssetImage('assets/cmd.png'),
+                      height: 27.0,
+                      width: 27.0,
+                    ),
                     Text(
                       "C:\\WINDOWS\\system32\\portfolio.exe",
                       style: TextStyle(color: Colors.white),
